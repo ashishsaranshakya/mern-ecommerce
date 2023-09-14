029fadcd5741f49158a15c669c34e8c5301ac4cf
Ashish Saran Shakya <harshshakya18585@gmail.com>
2023-09-14 19:18:19 +0530

    update logger timestamp formatting

--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,22 @@
 import winston from 'winston';
+import moment from 'moment-timezone';
+
+const timezone = process.env.TZ || 'Asia/Chennai';
+
+const customTimestamp = () => {
+    const now = moment().tz(timezone);
+    const offset = now.utcOffset();
+    const offsetSign = offset >= 0 ? '+' : '-';
+    const offsetHours = Math.abs(Math.floor(offset / 60));
+    const offsetMinutes = Math.abs(offset % 60);
+
+    return now.format(`DD-MM-YYYY HH:mm:ss.SSS UTC${offsetSign}${offsetHours}:${offsetMinutes}`);
+};
 
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
-        winston.format.timestamp(),
+        winston.format.timestamp({ format: customTimestamp }),
         winston.format.json()
     ),
     defaultMeta: { service: 'cittaa-backend' },
@@ -11,7 +24,7 @@ const logger = winston.createLogger({
         new winston.transports.Console(),
         new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
         new winston.transports.File({ filename: 'logs/combined.log' }),
-    ],
+    ]
 });
 
 export default logger;
